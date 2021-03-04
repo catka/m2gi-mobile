2d8ad09a4f319976601c1715aeee838496dccb0b
Richard Harding <hardingr@ill.fr>
2021-03-04 22:27:12 +0100

    forgot to add guard files

---
+++ src/app/guard/auth-guard.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable, NgZone } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private auth: AuthService, private route: Router, private ngZone: NgZone) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.auth.getConnectedUser().pipe(map(
+        (user: any) => {
+          if(!user || !user.emailVerified){
+            this.ngZone.run(() => this.route.navigate(['login']))
+          }
+          return true;
+        }
+    ));
+  }
+
+}
