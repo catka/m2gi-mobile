fd365e0a1dd83b138bd2bc7da114817773a4801a
Charlotte ROSSI <charlotte.rossi@tknoweb.fr>
2021-03-21 20:10:58 +0100

    add: list sharer name and picture on top of todo list

--- src/app/pages/list-details/list-details.module.ts
+++ src/app/pages/list-details/list-details.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ListDetailsPageRoutingModule } from './list-details-routing.module';
 
 import { ListDetailsPage } from './list-details.page';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ListDetailsPageRoutingModule
+    ListDetailsPageRoutingModule,
+    TranslateModule
   ],
   declarations: [ListDetailsPage]
 })
